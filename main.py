dbbd6a0e8592d9262ee77b891f6b2dc3fe2e1144
Alejandra <alzambranolu@unal.edu.co>
2024-11-12 21:57:06 -0500

    quick fix

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from agentlab.agents.generic_agent import AGENT_CUSTOM, RANDOM_SEARCH_AGENT, AGE
 from agentlab.analyze.inspect_results import get_most_recent_folder
 from agentlab.experiments import study_generators
 from agentlab.experiments.exp_utils import RESULTS_DIR
-from agentlab.experiments.launch_exp import make_study_dir, relaunch_study, run_experiments
+from agentlab.experiments.launch_exp import  relaunch_study, run_experiments
+from agentlab.analyze.inspect_results import get_most_recent_folder
+from agentlab.experiments.study_generators import run_agents_on_benchmark, Study
 from agentlab.experiments import task_collections as tasks
 from agentlab.experiments.launch_exp import import_object
 from browsergym.experiments.loop import EnvArgs, ExpArgs
@@ -35,26 +37,34 @@ config='CP'
 # benchmark = "workarena.l3"
 benchmark = "webarena"
 
+log_level=logging.DEBUG
+
 study_name= ""
 env_args_list = args.CrossProd(tasks.get_benchmark_env_args(
         benchmark, meta_seed=43, max_steps=None, n_repeat=None
     ))
-exp_args = None
+study = None
 if  config == 'CP':
-        study_name= 'multiagent_CP',
-        exp_args = args.expand_cross_product(
+    study = Study(
+        benchmark_name = benchmark,
+        agent_names = 'multiagent_CP',
+        exp_args_list = args.expand_cross_product(
             MultiAgentExpArgsCP(
                 agents_dict= {
                     'PLANNER':agents.planner_controller.PLAN_AGENT,
                     'CONTROLLER': agents.planner_controller.CONTROLLER_AGENT
                 },
                 env_args= env_args_list,
-                logging_level=logging.DEBUG,
+                logging_level=log_level
             )
-        )
+        ),
+        
+    )
 
 if config == 'CPO':
-        study_name= 'multiagent_CPO'
+    study = Study(    
+        benchmark_name = benchmark,
+        agent_names = 'multiagent_CPO',
         exp_args = args.expand_cross_product(
                 MultiAgentExpArgsCPO(  
                 agents_dict= {
@@ -63,42 +73,51 @@ if config == 'CPO':
                     'CONTROLLER': agents.cont_plan_obs.CONTROLLER_AGENT,
                 },
                 env_args= env_args_list,
-                logging_level=logging.DEBUG,
-            )
-        )
+                logging_level=log_level
+            )          
+        ),
+    )
+
 if config == 'generic':
-        study_name= 'generic_agent_4o_mini'
-        agent_args = import_object('agentlab.agents.generic_agent.AGENT_4o_MINI')
+    study = Study( 
+        benchmark_name = benchmark,
+        study_name= 'generic_agent_4o_mini',  
         exp_args = args.expand_cross_product(
                ExpArgs(
-                      agent_args=agent_args, 
+                      agent_args= import_object('agentlab.agents.generic_agent.AGENT_4o_MINI') ,
                       env_args= env_args_list, 
-                      logging_level=logging.DEBUG
+                      logging_level=log_level,
                       )
-                )
+                ),     
+        logging_level=log_level,
+    )
 
 
+# Set reproducibility_mode = True for reproducibility
+# this will "ask" agents to be deterministic. Also, it will prevent you from launching if you have
+# local changes. For your custom agents you need to implement set_reproducibility_mode
+reproducibility_mode = False 
 
-
-
-## select the kind of experiment (study)
-## Or define new studies, you only have to return list of ExpArgs to run and a name for the study
-#study_name, exp_args_list = study_generators.run_agents_on_benchmark(agent_args, benchmark)
-# study_name, exp_args_list = study_generators.ablation_study(agent, benchmark)
-# study_name, exp_args_list = study_generators.random_search(agent, benchmark, n_samples=20)
-
-study_dir = make_study_dir(RESULTS_DIR, study_name)
-
-
-## alternatively, relaunch an existing study
-# study_dir = get_most_recent_folder(RESULTS_DIR, contains=None)
-exp_args_list, study_dir = relaunch_study(study_dir, relaunch_mode="incomplete_or_error")
-
+# Set relaunch = True to relaunch an existing study, this will continue incomplete
+# experiments and relaunch errored experiments
+relaunch = False
 
 ## Number of parallel jobs
 n_jobs = 4  # Make sure to use 1 job when debugging in VSCode
 # n_jobs = -1  # to use all available cores
 
-# run the experiments
-if __name__ == "__main__":
-    run_experiments(n_jobs, exp_args, study_dir)
+
+if __name__ == "__main__":  # necessary for dask backend
+
+    #TODO MAKE FUNCTION FOR REPRODUCTABILITY
+
+    if relaunch:
+        #  relaunch an existing study
+        study_dir = get_most_recent_folder()
+        study = study_generators.make_relaunch_study(study_dir, relaunch_mode="incomplete_or_error")
+
+    study.run(n_jobs=n_jobs, parallel_backend="joblib", strict_reproducibility=reproducibility_mode)
+
+    if reproducibility_mode:
+        study.append_to_journal(strict_reproducibility=True)
+
