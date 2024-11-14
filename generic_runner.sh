6dcde1ce76837572dd36b9195c5475d7a29f1b2d
Alejandra <alzambranolu@unal.edu.co>
2024-11-14 11:59:28 -0500

    fixes in main for running script

---
+++ generic_runner.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=generic_job 
+#SBATCH --partition=unkillable
+#SBATCH --time=05:00:00
+module load python/3.10 cuda/11.8
+
+export RESULTS_DIR="/home/mila/a/alejandra.zambrano/scratch"
+export OPENAI_API_KEY="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"
+
+export BASE_URL="http://ec2-54-196-45-7.compute-1.amazonaws.com"
+
+export WA_SHOPPING="$BASE_URL:7770/"
+export WA_SHOPPING_ADMIN="$BASE_URL:7780/admin"
+export WA_REDDIT="$BASE_URL:9999"
+export WA_GITLAB="$BASE_URL:8023"
+export WA_WIKIPEDIA="$BASE_URL:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing"
+export WA_MAP="$BASE_URL:3000"
+export WA_HOMEPAGE="$BASE_URL:4399"
+
+#Configuration for webarena-browsergym
+pip install browsergym-webarena
+#python -c "import nltk; nltk.download('punkt'); nltk.download('punkt_tab')"
+pip install -r requirements.txt
+
+echo BROWSER-GYM-DONE
+
+#Configuration for multiagent
+cd multi-web-agent
+python -m venv venv
+source venv/bin/activate
+cd ../AgentLab
+pip install -e .
+playwright install
+cd ../multi-web-agent
+pip install -r requirements.txt
+
+echo MULTIAGENT-DONE
+
+python3 main.py --config generic --n_jobs 4
+
+
