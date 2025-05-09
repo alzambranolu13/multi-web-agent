da92ec2cf4c4c7987ae5384d452eb096f88c8084
alzambranolu13 <alzambranolu@unal.edu.co>
2025-05-09 01:02:12 -0400

    model correction 4.1

--- utils/models.py
+++ utils/models.py
@@ -7,5 +7,5 @@ from agentlab.agents.generic_agent.agent_configs import FLAGS_GPT_4o
 
 
 
-chat_model_41=  OpenAIModelArgs(model_name="gpt-4.1-2025-04-14",max_total_tokens=128_000,max_input_tokens=128_000,max_new_tokens=16_384,vision_support=True)
+chat_model_41=  OpenAIModelArgs(model_name="gpt-4.1-mini",max_total_tokens=128_000,max_input_tokens=128_000,max_new_tokens=16_384,vision_support=True)
 AGENT_41_M = GenericAgentArgs(chat_model_args=chat_model_41,flags=FLAGS_GPT_4o)
